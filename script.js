6b469f4a5876e58f71a183693b1e588defb72f27
Jax Choong <choongjax@gmail.com>
2024-03-12 16:32:23 +0800

    Added UI,game scoring and win conditions

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function getComputerChoice(){
     number = Math.floor(Math.random() * 100) 
     if (number <= 33) {
@@ -11,26 +12,43 @@ function getComputerChoice(){
     }
 }
 
-function playRound(){
-    playerChoice = prompt('Rock, Paper or Scissors? ')
-    playerChoice = playerChoice.toUpperCase()
-    computerChoice = getComputerChoice()
+function playRound(playerChoice){
+    const results = document.querySelector("#results")
+    let computerScore = document.querySelector("#computerScore")
+    let playerScore = document.querySelector("#playerScore")
+    const resultText = document.querySelector("p")
+    let computerChoice = getComputerChoice()
     computerChoice = computerChoice.toUpperCase()
+    playerChoice = playerChoice.toUpperCase()
+
+    if(playerScore.textContent == 5|| computerScore.textContent == 5){
+        return 0
+    }
     if (playerChoice=== computerChoice){
-        console.log("Tie!")
+        resultText.textContent="Tie!"
     }
     else if ((playerChoice === 'ROCK' && computerChoice === 'SCISSORS') || (playerChoice === 'PAPER' && computerChoice === 'ROCK') || (playerChoice === 'SCISSORS' && computerChoice === 'PAPER') ){
-        console.log(`You Win! ${playerChoice} beats ${computerChoice}`)
+        resultText.textContent= `You Win! ${playerChoice} beats ${computerChoice}`
+        playerScore.textContent = parseInt(playerScore.textContent) + 1
+        if(playerScore.textContent == 5){
+            resultText.textContent = "Player Wins!"
+        }
     }
     else{
-        console.log(`You Lose! ${computerChoice} beats ${playerChoice}`)
+        resultText.textContent = `You Lose! ${computerChoice} beats ${playerChoice}`
+        computerScore.textContent = parseInt(computerScore.textContent) + 1
+        if(computerScore.textContent == 5){
+            resultText.textContent = "Computer Wins!"
+        }
     }
+    results.appendChild(resultText)
+    document.querySelector("#playerScore").textContent = playerScore.textContent;
+    document.querySelector("#computerScore").textContent = computerScore.textContent;     //Updates the values in the html
 }
 
-function playGame(){
-    for (let i = 0; i < 5 ; i++){
-        playRound()
-    }
-}
-
-playGame()
+const buttons = document.querySelectorAll('button')
+buttons.forEach((button)=>{
+    button.addEventListener("click", ()=>{
+        playRound(button.textContent)
+    })
+})
